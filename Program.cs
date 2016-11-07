53c1dabbe3c6019376abc3c3c906bbcd8e1f60e3
Mixa <mixa@example.com>
2016-11-07 21:34:45 +0100

    Commit in to banch master

--- Program.cs
+++ Program.cs
@@ -22,5 +22,10 @@ namespace TestGitProject
             Console.WriteLine("!!!!! Hmmmm !!!!!!");
 
         }
+
+        static void branch1()
+        {
+            Console.WriteLine("Its 3 branch. Woohoo ;)");
+        }
     }
 }
